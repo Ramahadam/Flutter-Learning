2aa2ba5484d64e6a4fd6f1a79a7f05c2bf54034e
Mohamed <ramy.adam33@gmail.com>
2026-02-08 12:37:48 +0400

    add gradient background

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,9 +4,25 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello flutter'), // <--- Added comma
-        ), // <--- Added comma
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 26, 2, 80),
+                const Color.fromARGB(255, 111, 67, 173),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+
+          child: const Center(
+            child: Text(
+              'Hello flutter',
+              style: TextStyle(color: Colors.white, fontSize: 40),
+            ), // <--- Added comma
+          ), // <--- Added comma
+        ),
       ), // <--- Added comma
     ),
   );
