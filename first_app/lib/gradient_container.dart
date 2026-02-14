51336792f9a310c153e684c37edb9b4ff19eda75
Mohamed <ramy.adam33@gmail.com>
2026-02-14 15:10:36 +0400

    spliting code accorss files

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 111, 67, 173),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+
+      child: const Center(
+        child: Text(
+          'Hello flutter',
+          style: TextStyle(color: Colors.white, fontSize: 40),
+        ), // <--- Added comma
+      ), // <--- Added comma
+    );
+  }
+}
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -6,31 +7,4 @@ void main() {
       home: Scaffold(body: GradientContainer()), // <--- Added comma
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 111, 67, 173),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-
-      child: const Center(
-        child: Text(
-          'Hello flutter',
-          style: TextStyle(color: Colors.white, fontSize: 40),
-        ), // <--- Added comma
-      ), // <--- Added comma
-    );
-  }
 }
