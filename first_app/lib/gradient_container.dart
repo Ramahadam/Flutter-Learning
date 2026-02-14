5ed606cf9df82f6ecaa3ba6eceb398f7fdccaa16
Mohamed <ramy.adam33@gmail.com>
2026-02-14 15:35:10 +0400

    outsource gradient position to veriables

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 111, 67, 173),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
 
