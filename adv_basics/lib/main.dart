f78f6450f498f920d85f0bbdfa32b2c219cfc634
Mohamed <ramy.adam33@gmail.com>
2026-02-24 23:24:11 +0400

    create home page widget

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(backgroundColor: Colors.purple, body: BgWrapper()),
+    ),
+  );
+}
+
+class BgWrapper extends StatelessWidget {
+  const BgWrapper({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Align(
+        alignment: AlignmentDirectional.bottomCenter,
+        child: Column(
+          children: [
+            Image.asset('assets/images/quiz-logo.png'),
+            ElevatedButton(onPressed: () => {}, child: Text('Start Quiz')),
+          ],
+        ),
+      ),
+    );
+  }
+}
