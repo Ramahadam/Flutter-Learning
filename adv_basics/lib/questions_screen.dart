f315df64993c5f07a99ac6ca74aef5c404cae5e7
Mohamed <ramy.adam33@gmail.com>
2026-03-02 23:20:39 +0400

    create questions screen skeleton

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -12,6 +12,18 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestinsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('The question......', style: TextStyle(color: Colors.white)),
+          SizedBox(height: 20),
+          ElevatedButton(onPressed: () => {}, child: Text('First answer')),
+          ElevatedButton(onPressed: () => {}, child: Text('Second answer')),
+          ElevatedButton(onPressed: () => {}, child: Text('Third answer')),
+        ],
+      ),
+    );
   }
 }
