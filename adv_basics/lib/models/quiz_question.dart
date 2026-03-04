821326a6e523a4f0050e37da4e7824e2f317aed0
Mohamed <ramy.adam33@gmail.com>
2026-03-04 22:33:42 +0400

    feat: add shuffle method in QuizeQuestion class

--- adv_basics/lib/models/quiz_question.dart
+++ adv_basics/lib/models/quiz_question.dart
@@ -3,4 +3,11 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffleAnswers() {
+    final shuffleAnswers = List.of(answers);
+    shuffleAnswers.shuffle();
+
+    return shuffleAnswers;
+  }
 }
