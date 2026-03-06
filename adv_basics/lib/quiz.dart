d855d6be8ec4df2663f2fc1794530dd1afe9776b
Mohamed <ramy.adam33@gmail.com>
2026-03-06 22:51:11 +0400

    feat: add result screen widget skeleton

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -14,10 +16,18 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
 
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
 
   void chooseScreen(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+
+        selectedAnswers = [];
+      });
+    }
   }
 
   void switchScreen() {
@@ -32,6 +42,10 @@ class _QuizState extends State<Quiz> {
 
     if (activeScreen == 'questions-screen') {
       screenWidget = QuestionsScreen(onSelectAnswer: chooseScreen);
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen();
     }
 
     return MaterialApp(
