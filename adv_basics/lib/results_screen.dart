ac9e87bc06a3bdcc657806acf8a7a09651ca16d5
Mohamed <ramy.adam33@gmail.com>
2026-03-10 13:17:53 +0400

    feat: create summary answers widget

--- adv_basics/lib/results_screen.dart
+++ adv_basics/lib/results_screen.dart
@@ -1,7 +1,25 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.choosenAnswers});
+  final List<String> choosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': choosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(context) {
@@ -12,7 +30,7 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("You answered X out of Y questions correctly"),
+            QuestionsSummary(summaryData: getSummaryData()),
             SizedBox(height: 30),
             Text('List of answers and questions'),
             SizedBox(height: 30),
