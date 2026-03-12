eeeee9a61f102a080da5e968681c46917f2eb769
Mohamed <ramy.adam33@gmail.com>
2026-03-12 23:19:14 +0400

    feat(result): display summary results

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -24,8 +24,6 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         activeScreen = 'results-screen';
-
-        selectedAnswers = [];
       });
     }
   }
