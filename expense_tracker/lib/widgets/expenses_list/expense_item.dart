43fe1f29ac06f01af1cd908404c0fd16e04dd8e3
Mohamed <ramy.adam33@gmail.com>
2026-04-11 14:30:03 +0400

    feat: create expense item widget

---
+++ expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,35 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                Spacer(),
+                Row(
+                  children: [
+                    Icon(Icons.alarm),
+                    SizedBox(width: 80),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
