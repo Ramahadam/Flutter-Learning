68edcc7fdb3392c4e2abe2f2f66d619ac498fd6c
Mohamed <ramy.adam33@gmail.com>
2026-04-11 14:49:55 +0400

    create category icons object

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,8 +1,16 @@
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.car_rental,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
