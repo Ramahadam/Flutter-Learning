4d90dc6ffc4d7d4f266b7e49545d8794e19d6760
Mohamed <ramy.adam33@gmail.com>
2026-04-13 14:33:07 +0400

    feat(expense): add model wdiget

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -25,12 +26,23 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) {
+        return NewExpense();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Expense Tracker"),
-        actions: [IconButton(onPressed: () => {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(onPressed: _openAddExenseOverlay, icon: Icon(Icons.add)),
+        ],
       ),
       body: Column(
         children: [
